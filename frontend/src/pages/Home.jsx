8b32173caa999ffefd95913abc4ef4dc264e6716
Joeysm272 <Joeysm272@gmail.com>
2025-02-27 13:29:27 -0600

    added Login and Home page

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+        HTML goes here 
+        CSS file is /src/index.css
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>
+        HTML goes here
+        CSS file is /src/index.css
+    </div>
+  )
+}
+
+export default Login
