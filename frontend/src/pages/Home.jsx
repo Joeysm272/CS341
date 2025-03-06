5eb79db8a9a58de87aac7fd16f2aaf8efcfc3672
Joeysm272 <Joeysm272@gmail.com>
2025-03-05 18:11:28 -0600

    added api

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,37 @@ import React from 'react'
 import Navbar from '../components/navbar'
 
 const Home = () => {
+
+//   const createProgram = async () => {
+//     fetch('http://localhost:8000/programs', {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json" // Telling the server we’re sending JSON
+//       },
+//       body: JSON.stringify({
+//         programName: 'Sharks',
+//         time: '8:00am',
+//         location: 'Mars',
+//         capacity: 10,
+//         price: 15,
+//         desc: 'Swimming lessons'
+//       })
+//     })
+//   }
+
+// const deleteProgram = async () => {
+//   fetch('http://localhost:8000/programs/67c7976a50fce705a4ca4286', {
+//     method: "DELETE"
+//   })
+// }
+        // <button className='bg-black text-white cursor-pointer' onClick={createProgram}>Create Program</button>
+        // <button className='bg-black text-white cursor-pointer' onClick={deleteProgram}>Delete Program</button>
   return (
     <div>
       <Navbar />
       HTML goes here 
       CSS file is /src/index.css
-        
+
     </div>
   )
 }
